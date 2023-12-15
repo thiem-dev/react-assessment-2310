cee222879cd09a3221b68cea1d3045d9a3bff874
Garrett Ross <garrettross@gGarrettRoss.attlocal.net>
2023-12-15 09:29:05 -0600

    Quiz

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
+import {useState} from 'react'
+import Products from './Components/Products'
 import './App.css';
 
 function App() {
+  
+  const [products, setProducts] = useState([
+    {
+      id: 19876,
+      productData: {
+        name: "Nike Hyperdrive 0x3",
+        pricing: 105, 
+        quarter: 3,
+        productiondata: {
+          region: "SW NAUS",
+          data: "12/01/2023"
+        }
+      }
+    },
+    {
+      id: 19877,
+      productData: {
+        name: "Under armor long sleeve essential collection",
+        pricing: 210, 
+        quarter: 2,
+        productiondata: {
+          region: "SW NAUS",
+          data: "12/07/2023"
+        }
+      }
+    },
+    {
+      id: 19878,
+      productData: {
+        name: "Nike kick back 2010",
+        pricing: 135, 
+        quarter: 2,
+        productiondata: {
+          region: "SW NAUS",
+          data: "11/02/2023"
+        }
+      }
+    },
+  ])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+
+    <>
+      <h1>Instructions</h1>
+      <p>Answer the questions in the ./components/questions.txt file, then move onto the ./components/Products.jsx file. Good luck.</p>
+
+      {/* Comonents to be rendered below */}
+      <Products />
+    </>
   );
 }
 
